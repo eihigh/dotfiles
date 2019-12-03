0a38fdee971818dbe36fc5432a2155eeb2aeff36
eihigh <eihigh.contact@gmail.com>
2019-12-03 16:50:25 +0900

    コンテナ移行続き

--- home/install.pl
+++ home/install.pl
@@ -1,15 +1,25 @@
 #!/usr/bin/env perl
 use strict;
+use warnings;
 use File::Basename;
 use File::Spec::Functions qw/catfile/;
 
 my $home = $ENV{HOME};
 my $dotfiles = $ENV{DOTFILES} or die 'no such env var: DOTFILES';
+my $dotbak = catfile($home, '.dotbak');
+`mkdir -p ${dotbak}`;
 
-for my $dst (glob "${dotfiles}/home/*") {
-	my $name = basename($dst);
+for my $old (glob "${dotfiles}/home/*") {
+	my $name = basename($old);
 	next if $name eq 'install.pl'; # do not symlink myself
+	$name = '.'.$name;
 
-	my $src = catfile($home, '.'.$name);
-	symlink $dst, $src;
+	# make backup
+	my $new = catfile($home, $name);
+	if (-e $new) {
+		`mv ${new} ${dotbak}/${name}`;
+	}
+
+	# symlink
+	symlink $old, $new;
 }
