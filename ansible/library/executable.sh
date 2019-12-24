88bf778466e73e205fd922b5312d76d5306f2478
eihigh <eihigh.contact@gmail.com>
2019-12-24 18:51:32 +0900

    update

---
+++ ansible/library/executable.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+source `dirname $0`/args
+if which $name > /dev/null ; then
+	echo '{"changed": false}'
+else
+	msg=$(sh -c "$action" 2>&1 > /dev/null) && f='' || f=', "failed": true'
+	echo "{\"changed\": true, \"msg\": \"$msg\"$f}"
+fi
