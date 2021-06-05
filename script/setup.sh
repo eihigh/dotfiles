4a85a94f55ee01badd53829a8816e93d86db4ca0
eihigh <eihigh.contact@gmail.com>
2021-06-05 06:10:47 +0000

    update

---
+++ script/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir -p ~/.cache/vim
+mkdir -p ~/.vim
+mkdir -p ~/.vim/plugged
+mkdir -p ~/.vim/autoload
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -s ~/dotfiles/settings/anyshrc ~/.anyshrc
+ln -s ~/dotfiles/settings/bashrc ~/.bashrc
+ln -s ~/dotfiles/settings/sshrc ~/.sshrc
+ln -s ~/dotfiles/settings/sshrc.d ~/.sshrc.d
+ln -s ~/dotfiles/settings/vimrc ~/.vimrc
+ln -s ~/dotfiles/settings/zshrc ~/.zshrc
+ln -s ~/dotfiles/settings/gitconfig_shared ~/.gitconfig_shared
+ln -s ~/dotfiles/settings/gitignore_global ~/.gitignore_global
+
+yes | vim +PlugInstall +qall
+
+git config --global include.path ~/.gitconfig_shared
+
+go install golang.org/x/tools/gopls@latest
+go install golang.org/x/tools/cmd/goimports@latest
+go install github.com/mattn/gof@latest
+go install github.com/x-motemen/ghq@latest
+# go install github.com/jesseduffield/lazygit@latest
+go get github.com/jesseduffield/lazygit
+go install github.com/monochromegane/the_platinum_searcher/cmd/pt@latest
+
+sudo npm install -g typescript
---
+++ script/setup_arch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+pacman-key --init
+pacman-key --populate archlinux
+pacman -Syy archlinux-keyring
+pacman -Syu
+
+ln -fs /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+
+echo 'ja_JP.UTF-8 UTF-8' >> /etc/locale.gen
+locale-gen
+echo 'LANG=ja_JP.UTF-8' > /etc/locale.conf
+
+useradd -m -G wheel me
+echo me:me | chpasswd
+echo "me ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+pacman -Syy --noconfirm sudo which openssh git go jq vim zsh nodejs npm
