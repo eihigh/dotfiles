3cd2a7a10c7819018b41f382f721f571b0fe8856
eihigh <eihigh.contact@gmail.com>
2023-03-02 13:36:48 +0900

    update

--- run_after_always.sh
+++ run_after_always.sh
@@ -7,3 +7,6 @@ if [ "$SHELL" != *"/zsh" ]; then
 	grep '/zsh' < /etc/shells || which zsh | sudo tee -a /etc/shells
 	sudo chsh -s $(which zsh) $USER
 fi
+
+# Include my gitconfig
+git config --global include.path ~/.gitconfig_shared
