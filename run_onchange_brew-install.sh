1422a0dc7efd935e791bc1ca12379b8b3548373a
eihigh <eihigh.contact@gmail.com>
2024-07-12 00:35:57 +0900

    script: ignore errors on installing with brew
    時々 brew install がこけるがその後の手順に支障ないようにする

--- run_onchange_brew-install.sh
+++ run_onchange_brew-install.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 [ -d /home/linuxbrew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 [ -d /opt/homebrew ] && eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew install chezmoi go jq vim zsh fzf node gopls gh ghq lazygit ripgrep
+brew install chezmoi go jq vim zsh fzf node gopls gh ghq lazygit ripgrep || true
